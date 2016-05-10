93e1fbc5668587bde202f8351855f1ea368e9601
SergioAri <sergioarizaga@outlook.com>
2016-05-10 12:43:42 -0500

    t

--- app/src/test/java/com/example/iw/themesample/MainActivityTest.java
+++ app/src/test/java/com/example/iw/themesample/MainActivityTest.java
@@ -1,15 +1,12 @@
 package com.example.iw.themesample;
 
 import android.test.ActivityInstrumentationTestCase2;
-import android.widget.TextView;
 
 /**
  * Created by iw on 05/05/16.
  */
-public class MainActivityTest extends
-        ActivityInstrumentationTestCase2<MainActivity> {
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
-    private MainActivity mTestActivity;
 
     public MainActivityTest() {
         super(MainActivity.class);
@@ -17,14 +14,13 @@ public class MainActivityTest extends
 
     @Override
     protected void setUp() throws Exception {
-//        super.setUp();
+        super.setUp();
 
         // Starts the activity under test using
         // the default Intent with:
         // action = {@link Intent#ACTION_MAIN}
         // flags = {@link Intent#FLAG_ACTIVITY_NEW_TASK}
         // All other fields are null or empty.
-      //  mTestActivity = getActivity();
 
     }
 
@@ -40,7 +36,6 @@ public class MainActivityTest extends
         // These messages will be shown if
         // a tests fails and make it easy to
         // understand why a test failed
-        //assertNotNull("mTestActivity is null", mTestActivity);
     }
 
 }
